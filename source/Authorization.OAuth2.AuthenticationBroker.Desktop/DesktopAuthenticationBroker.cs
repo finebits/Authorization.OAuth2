f7202cf87c208728501684c37fb347780646cd9a
alexander <alex+github@finebits.com>
2024-11-15 20:24:54 +0200

    style: fix IDE0005
    Remove unnecessary import

--- source/Authorization.OAuth2.AuthenticationBroker.Desktop/DesktopAuthenticationBroker.cs
+++ source/Authorization.OAuth2.AuthenticationBroker.Desktop/DesktopAuthenticationBroker.cs
@@ -17,7 +17,6 @@
 // ---------------------------------------------------------------------------- //
 
 using System;
-using System.Collections.Specialized;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
